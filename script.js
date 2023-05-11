e0146977628623875777bac5fcb7adf8c85b42f0
Tatien Miller <tatmil99@gmail.com>
2023-05-11 05:06:53 -0500

    getComputerChoice function is working and returns correct values.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getComputerChoice() {}
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * (4 - 1) + 1);
+
+  return randomNum == 1
+    ? "Rock"
+    : randomNum == 2
+    ? "Paper"
+    : randomNum == 3
+    ? "Scissors"
+    : NaN;
+}
 
 function getPlayerChoice() {}
 
