df78291a1e6447566b039a511b5c99e3fd1d517b
Tatien Miller <tatmil99@gmail.com>
2023-05-11 19:12:11 -0500

    Function playRound is finished but not yet tested.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//playRound(getPlayerChoice, getComputerChoice);
+playRound(getPlayerChoice, getComputerChoice);
 
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * (4 - 1) + 1);
@@ -12,12 +12,31 @@ function getComputerChoice() {
     : NaN;
 }
 
+function validatePlayerChoice() {} // Ensure player enters only valid choices
+
 function getPlayerChoice() {
   const choice = prompt("Choose 'Rock', 'Paper', or 'Scissors':").toLowerCase();
 
   return choice;
 }
 
-function playRound(playerChoice, computerChoice) {}
+function playRound(playerChoice, computerChoice) {
+  const player = playerChoice();
+  const computer = computerChoice();
+  console.log(`Player's choice: ${player}`);
+  console.log(`Computers's choice: ${computer}`);
+
+  if (player == computer) {
+    return "Tie";
+  } else if (player == "rock" && computer != "paper") {
+    return "Player wins";
+  } else if (player == "paper" && computer != "scissors") {
+    return "Player wins";
+  } else if (player == "scissors" && computer != "rock") {
+    return "Player wins";
+  } else {
+    return "Computer wins";
+  }
+}
 
 function game() {}
